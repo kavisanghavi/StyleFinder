c5bcc7b2f8fb2dbc3cbbc7f0c978c9081f43e130
jenishkananiLime <jenish.kanani@li.me>
2025-11-18 00:29:30 -0500

    Initial Commit

---
+++ ClosetAI/ClosetAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetAIApp.swift
+//  ClosetAI
+//
+//  Created by AI PC on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ClosetAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClosetAITests/ClosetAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetAITests.swift
+//  ClosetAITests
+//
+//  Created by AI PC on 11/18/25.
+//
+
+import Testing
+@testable import ClosetAI
+
+struct ClosetAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
